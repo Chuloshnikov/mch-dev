d51fb6effdf6798ddaecbf92f5b20811aa13c862
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2025-06-20 14:05:13 +0300

    +

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import AboutSection from "@/components/AboutSection";
 import ContactsSection from "@/components/ContactsSection";
 import ExperienceSection from "@/components/ExperienceSection";
-import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import SkillsSection from "@/components/SkillsSection";
